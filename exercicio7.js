b89efa995e9b727f1aff680f3b3df6a90611af9a
Mariane Monteiro <marimonteironasciment@gmail.com>
2022-04-15 13:44:44 -0300

    atualização dos exercícios da semana

--- exercicio7.js
+++ exercicio7.js
@@ -1,1 +1,21 @@
 //Crie uma função que recebe 2 parâmetros e retorna o resultado da divisão entre eles. Diga se esse numero é par ou ímpar
+let resultado = 0
+
+function dividir(num1, num2){
+    resultado = num1/num2 
+    return resultado
+}
+
+function paridade(numero){
+    
+    if (numero % 2 == 0){
+        return "o número " + numero + " é par"
+    }
+    else {
+        return "o número " + numero + " é impar"
+    }
+}
+
+console.log(dividir(27,3.6))
+
+console.log(paridade(resultado))
